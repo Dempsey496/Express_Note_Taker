37427c90a368051bd41a7bb03be214011557f9a8
Dempsey FP <dempseyfinley@gmail.com>
2020-11-28 15:13:17 -0500

    view routes created

--- server.js
+++ server.js
@@ -8,10 +8,31 @@ const app = express();
 // Adding a Port assigned by Heroku, or 8080 if avaliable
 const PORT = process.env.PORT || 3000;
 
+// Sets up data parsing middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Adding a test route
 app.get("/api/config", (req, res) => {
     res.json("Route created");
 });
+
+// Get route for notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Get route for home page
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Get route for reading db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+});
+// NEEDS WORK
+
 
 // Listening on assigned Port
 app.listen(PORT, () => {
